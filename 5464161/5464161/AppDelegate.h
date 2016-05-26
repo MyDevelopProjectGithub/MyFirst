c944f6c0d91a2c6ff106c2a5276bbf510ec8dfb8
lanouhn <lanouhn@50-2.local>
2016-05-26 17:49:07 +0800

    lip

---
+++ 5464161/5464161/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  5464161
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 李华东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 5464161/5464161/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  5464161
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 李华东. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
